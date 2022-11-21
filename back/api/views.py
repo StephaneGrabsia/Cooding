14f9e4668390d1c96fb4a94f5864d150121c13a6
LiBekka <lina.bekkali@eleves.enpc.fr>
2022-11-21 16:23:24 +0100

    api first test model

--- back/api/views.py
+++ back/api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         {
@@ -11,5 +13,29 @@ def getRoutes(request):
             'body': None,
             'description': 'Returns an array of notes'
         },
+        {
+            'Endpoint' : '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'
+        },
+        {
+            'Endpoint' : '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates a new note with data sent in post request'
+        },
+        {
+            'Endpoint' : '/notes/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing note with data sent in post request'
+        },        
+        {
+            'Endpoint' : '/notes/create/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes an existing route'
+        },
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
