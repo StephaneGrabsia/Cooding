11660d1064cc614a2662a02d8a46cd36ae52dfa9
StephaneGrabsia <stephane.grabsia@eleves.enpc.fr>
2022-11-27 23:26:08 +0100

    Connecting the API with the front with a simple test

--- back/uQuizz/settings.py
+++ back/uQuizz/settings.py
@@ -61,7 +61,9 @@ CORE_APPS = [
 
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = [
+    "corsheaders",
+]
 
 PROJECT_APPS = []
 
@@ -70,11 +72,13 @@ INSTALLED_APPS = CORE_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
 ]
 
 ROOT_URLCONF = "uQuizz.urls"
@@ -97,6 +101,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "uQuizz.wsgi.application"
 
+#CORS Headers$
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8080/",
+# ]
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import ReactDOM from "react-dom";
 // import './styles/style.css'
 import './styles/style.scss'
 
 const App = () => {
-    return <h1>Hello from react and sass</h1>
+    let [description, setDescription] = useState([]);
+
+    useEffect(() => {
+        getNotes()
+    })
+
+    let getNotes = async () => {
+        let response = await fetch("http://localhost:8000/");
+        let data = await response.json();
+        console.log("DATA : ", data);
+        setDescription(data[0]["description"]);
+    }
+    // console.log({description})
+    return(<h1>Voici quelque chose que j'ai récupéré sur l'API : {description}</h1>)
 }
 
 ReactDOM.render(<App />, document.getElementById("root"))
