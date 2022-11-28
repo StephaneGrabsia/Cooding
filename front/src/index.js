d616a7630a419a7e7a31405eb9ac6ecf7e2986dd
StephaneGrabsia <stephane.grabsia@eleves.enpc.fr>
2022-11-28 21:48:52 +0100

    fixing the linter, and some js files

--- front/src/index.js
+++ front/src/index.js
@@ -1,23 +1,25 @@
-import React, { useState, useEffect} from "react";
-import ReactDOM from "react-dom";
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
 // import './styles/style.css'
-import './styles/style.scss'
+import './styles/style.scss';
 
 const App = () => {
-    let [description, setDescription] = useState([]);
+  const [description, setDescription] = useState([]);
 
-    useEffect(() => {
-        getNotes()
-    })
+  useEffect(() => {
+    getNotes();
+  });
 
-    let getNotes = async () => {
-        let response = await fetch("http://localhost:8000/");
-        let data = await response.json();
-        console.log("DATA : ", data);
-        setDescription(data[0]["description"]);
-    }
-    // console.log({description})
-    return(<h1>Voici quelque chose que j'ai récupéré sur l'API : {description}</h1>)
-}
+  const getNotes = async () => {
+    const response = await fetch('http://localhost:8000/');
+    const data = await response.json();
+    console.log('DATA : ', data);
+    setDescription(data[0]['description']);
+  };
+  // console.log({description})
+  return (<h1>Coucou Hector.
+    Voici quelque chose que j'ai récupéré sur l'API : {description}
+  </h1>);
+};
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById('root'));
--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,36 +1,36 @@
 const path = require('path');
 
 module.exports = {
-    devtool: 'eval-source-map',
-    mode: 'development',
-    entry: {
-        index: './src/index.js',
-    },
-    resolve: {
-        modules: [__dirname, 'node_modules'],
-        extensions: ['.js', '.jsx']
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: '[name].bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                // if use css
-                // test: /\.css$/,
-                // use: ['style-loader', 'css-loader']
+  devtool: 'eval-source-map',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  resolve: {
+    modules: [__dirname, 'node_modules'],
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        // if use css
+        // test: /\.css$/,
+        // use: ['style-loader', 'css-loader']
 
-                // if use scss
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+        // if use scss
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
 
-            }
-        ]
-    }
+      },
+    ],
+  },
 };
