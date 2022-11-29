b481659fdfd36fcff4ffb95862466b96a9ad6bed
SullivanENPC <sullivan.castro@eleves.enpc.fr>
2022-11-29 15:58:15 +0100

    tests simples sur Exercise et Member

--- back/api/tests.py
+++ back/api/tests.py
@@ -1,14 +1,21 @@
 from django.test import TestCase
-from api.models import Member
+from api.models import *
 
 # Create your tests here.
-class MembersTestCase:
+class MemberTestCase(TestCase):
     def setUp(self):
         Member.objects.create(username='Sullivan')
-        Member.objects.create(username='s'*500)
 
     def test_assert_username(self):
         sullivan = Member.objects.get(username="Sullivan")
-        snake = Member.objects.get(username='s'*500)
         self.assertEqual(str(sullivan), "Sullivan")
-        self.assertEqual(str(snake), 's'*50)
+
+class ExerciseTestCase(TestCase):
+    def setUp(self):
+        Exercise.objects.create(subject='Fonction carré', 
+            solution="def f(x):\n return x**2", test_input="[0, 1, 2, 3, 4, 5]", 
+            correct_output="[0, 1, 4, 9, 16, 25]")
+    
+    def test_assert_exercise(self):
+        exercise1 = Exercise.objects.get(subject='Fonction carré')
+        self.assertEqual(str(exercise1), 'Fonction carré')
