a7f97250b2e5610ec44a2d918b18f50b63c9613c
SullivanENPC <sullivan.castro@eleves.enpc.fr>
2022-11-29 15:43:38 +0100

    actions test models

--- back/api/tests.py
+++ back/api/tests.py
@@ -6,3 +6,9 @@ class MembersTestCase:
     def setUp(self):
         Member.objects.create(username='Sullivan')
         Member.objects.create(username='s'*500)
+
+    def test_assert_username(self):
+        sullivan = Member.objects.get(username="Sullivan")
+        snake = Member.objects.get(username='s'*500)
+        self.assertEqual(str(sullivan), "Sullivan")
+        self.assertEqual(str(snake), 's'*50)
