a3744c2349ce9fcdc8c81ba984fb87d9d94f3d7a
StephaneGrabsia <stephane.grabsia@eleves.enpc.fr>
2022-12-04 18:22:10 +0100

    Memoization of getNotes and useEffect()

--- front/src/index.js
+++ front/src/index.js
@@ -1,23 +1,30 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect, useCallback} from "react";
 import ReactDOM from "react-dom";
 // import './styles/style.css'
 import './styles/style.scss'
 
+
+
 const App = () => {
     let [description, setDescription] = useState([]);
 
-    useEffect(() => {
-        getNotes()
-    })
-
-    let getNotes = async () => {
+    const getNotes = useCallback(
+      async () => {
         let response = await fetch("http://localhost:8000/");
         let data = await response.json();
         console.log("DATA : ", data);
         setDescription(data[0]["description"]);
-    }
+      },
+      [setDescription]
+    ); 
+
+    useEffect(() => {
+        getNotes();
+    }, [setDescription, getNotes])
+
     // console.log({description})
     return(<h1>Voici quelque chose que j'ai récupéré sur l'API : {description}</h1>)
 }
+
 
 ReactDOM.render(<App />, document.getElementById("root"))
