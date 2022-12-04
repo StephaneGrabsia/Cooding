073628f46655ffe3da511ba626d32798a94a73c3
StephaneGrabsia <stephane.grabsia@eleves.enpc.fr>
2022-12-04 16:55:15 +0100

    Importing the right module in CheckboxWithLabel

--- front/src/components/CheckboxWithLabel.js
+++ front/src/components/CheckboxWithLabel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useState} from 'react';
+import PropTypes from 'prop-types';
 
 export const CheckboxWithLabel = ({labelOn, labelOff}) => {
   const [isChecked, setIsChecked] = useState(false);
