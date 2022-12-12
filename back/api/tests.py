749ca9ae5d7b441966879be2098ab17bf650b46d
SullivanENPC <sullivan.castro@eleves.enpc.fr>
2022-12-12 14:39:13 +0100

    models Classroom, Exercise, Teacher, Solution

--- back/api/tests.py
+++ back/api/tests.py
@@ -19,20 +19,20 @@ import os
 #         sullivan = Teacher.objects.get(username="Sullivan")
 #         self.assertEqual(str(sullivan), "Sullivan")
 
-class ExerciseTestCase(TestCase):
-    def setUp(self):
-        Exercise.objects.create(statement='Fonction carré', 
-            solution="def f(x):\n   return x**2", test_input="[0, 1, 2, 3, 4, 5]", 
-            correct_output="[0, 1, 4, 9, 16, 25]")
-        Exercise.objects.create(statement='Fonction cube', 
-            solution="def f(x):\n   return 2*x**3\n", test_input="[0, 1, 2, 3, 4, 5]", 
-            correct_output="[0, 1, 8, 27, 64, 125]")
+# class ExerciseTestCase(TestCase):
+#     def setUp(self):
+#         Exercise.objects.create(statement='Fonction carré', 
+#             solution="def f(x):\n   return x**2", test_input="[0, 1, 2, 3, 4, 5]", 
+#             correct_output="[0, 1, 4, 9, 16, 25]")
+#         Exercise.objects.create(statement='Fonction cube', 
+#             solution="def f(x):\n   return 2*x**3\n", test_input="[0, 1, 2, 3, 4, 5]", 
+#             correct_output="[0, 1, 8, 27, 64, 125]")
     
-    def test_assert_exercise(self):
-        exercise1 = Exercise.objects.get(statement='Fonction carré')
-        self.assertEqual(str(exercise1), 'Fonction carré')
-        self.assertEqual(exercise1.run(), "[0, 1, 4, 9, 16, 25]\n")
-        self.assertEqual(exercise1.check_sol(exercise1.run()), True)
-        exercise2 = Exercise.objects.get(statement='Fonction cube')
-        self.assertNotEqual(exercise2.run(), "[0, 1, 8, 27, 64, 125]\n")
-        self.assertEqual(exercise2.check_sol(exercise2.run()), False)
+#     def test_assert_exercise(self):
+#         exercise1 = Exercise.objects.get(statement='Fonction carré')
+#         self.assertEqual(str(exercise1), 'Fonction carré')
+#         self.assertEqual(exercise1.run(), "[0, 1, 4, 9, 16, 25]\n")
+#         self.assertEqual(exercise1.check_sol(exercise1.run()), True)
+#         exercise2 = Exercise.objects.get(statement='Fonction cube')
+#         self.assertNotEqual(exercise2.run(), "[0, 1, 8, 27, 64, 125]\n")
+#         self.assertEqual(exercise2.check_sol(exercise2.run()), False)
