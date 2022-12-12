20f3796befb8846874a8abd2d529ee8170151e81
sachabinder <binder.sacha@gmail.com>
2022-12-12 16:07:28 +0100

    background

--- front/src/app.js
+++ front/src/app.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SignInOutContainer from "./containers";
-
-//import './styles/style.scss'
+import './styles/style.scss'
 
 
 const App = () => {
     return (
         <div className="app">
-            <SignInOutContainer/>
+            <SignInOutContainer />
         </div>
     )
 }
