34684217ba7b2f1d9b651a7a115c0cb0939863f4
sachabinder <binder.sacha@gmail.com>
2022-12-21 14:24:55 +0100

    lint

--- back/api/admin.py
+++ back/api/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Classroom, Exercise, Solution
+from api.models import User, Teacher, Classroom, Exercise, Solution
 
 admin.site.register(User)
+admin.site.register(Teacher)
 admin.site.register(Classroom)
 admin.site.register(Exercise)
 admin.site.register(Solution)
-
-
--- back/api/urls.py
+++ back/api/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from . import views
-from .views import *
+from api.views import (
+    getRoutes,
+    TeacherRegisterView,
+    TeacherLoginView,
+    TeacherView,
+    LogoutView,
+)
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes"),
-    path('register/', UserRegisterView.as_view()),
-    path('teacher/register/', TeacherRegisterView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('teacher/login/', TeacherLoginView.as_view()),
-    path('user/', UserView.as_view()),
-    path('teacher/', TeacherView.as_view()),
-    path('logout/', LogoutView.as_view()),
-
+    path("", getRoutes, name="routes"),
+    path("teacher/register/", TeacherRegisterView.as_view()),
+    path("teacher/login/", TeacherLoginView.as_view()),
+    path("teacher/", TeacherView.as_view()),
+    path("logout/", LogoutView.as_view()),
 ]
