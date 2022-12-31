51f919ad2d0e1209a7628b49addc62264877072c
LiBekka <lina.bekkali@eleves.enpc.fr>
2022-12-31 16:59:12 +0100

    Exercise views test

--- back/api/tests.py
+++ back/api/tests.py
@@ -162,7 +162,7 @@ class ExerciseTestCase(TestCase):
             gender="Femme",
         )
         room = Classroom.objects.create(
-            room_id = 79,
+            room_id = 77,
             teacher = teacher
         )
         Exercise.objects.create(
@@ -177,38 +177,59 @@ class ExerciseTestCase(TestCase):
         exercise = Exercise.objects.get(statement="Fonction carrée")
         self.assertIsInstance(exercise, Exercise)
         self.assertEqual(str(exercise), "Fonction carrée")
+        self.assertEqual(exercise1.run(), "[0, 1, 4, 9, 16, 25]\n")
+        self.assertEqual(exercise1.check_sol(exercise1.run()), True)
 
-# class ExerciseTestCase(TestCase):
-#     def setUp(self):
-#         Exercise.objects.create(statement='Fonction carré',
-#             solution="def f(x):\n   return x**2", test_input="[0, 1, 2, 3, 4, 5]",
-#             correct_output="[0, 1, 4, 9, 16, 25]")
-#         Exercise.objects.create(statement='Fonction cube',
-#             solution="def f(x):\n   return 2*x**3\n", test_input="[0, 1, 2, 3, 4, 5]",
-#             correct_output="[0, 1, 8, 27, 64, 125]")
+    def test_api_Exercise_create_fetch_delete(self):
+        c = Client()
+        c.post(
+            "/teacher/register/",
+            {
+                "user": {"username": "help", "password": "please"}, 
+                "first_name": "help", 
+                "last_name": "help", 
+                "gender": "Femme"
+            },
+            content_type="application/json"
+        )
+        c.post(
+            "/teacher/login/",
+            {"username": "help", "password": "please"}
+        )
+        c.post(
+            "/room/create/", 
+            {
+                "room_id":85,
+                "teacher":1
+            }, 
+            content_type="application/json")
+        response = c.post(
+            "/exercise/create/", 
+            {
+                "statement": "Fonction double",
+                "solution": "def f(x):\n return 2*x",
+                "test_input": "[0, 1, 2, 3, 4, 5]",
+                "correct_output": "[0, 2, 4, 6, 8, 10]",
+                "classroom": 85
+            },
+            content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        response = c.post(
+            "/exercise/", 
+            {
+                "statement": "Fonction double"
+            },
+            content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        response = c.post(
+            "/room/delete/", 
+            {
+                "statement":"Fonction double"
+            }, 
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 200)
 
-#     def test_assert_exercise(self):
-#         exercise1 = Exercise.objects.get(statement='Fonction carré')
-#         self.assertEqual(str(exercise1), 'Fonction carré')
-#         self.assertEqual(exercise1.run(), "[0, 1, 4, 9, 16, 25]\n")
-#         self.assertEqual(exercise1.check_sol(exercise1.run()), True)
-#         exercise2 = Exercise.objects.get(statement='Fonction cube')
-#         self.assertNotEqual(exercise2.run(), "[0, 1, 8, 27, 64, 125]\n")
-#         self.assertEqual(exercise2.check_sol(exercise2.run()), False)
 
-# class StudentTestCase(TestCase):
-#     def setUp(self):
-#         Student.objects.create(username='Sullivan')
 
-#     def test_assert_username(self):
-#         sullivan = Student.objects.get(username="Sullivan")
-#         self.assertEqual(str(sullivan), "Sullivan")
-
-# class TeacherTestCase(TestCase):
-#     def setUp(self):
-#         Teacher.objects.create(username='Sullivan')
-
-#     def test_assert_username(self):
-#         sullivan = Teacher.objects.get(username="Sullivan")
-#         self.assertEqual(str(sullivan), "Sullivan")
 
