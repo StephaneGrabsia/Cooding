b5a202f3e2d54313d02406013495b351d5825e3b
Sacha BINDER <sacha@Sachas-Laptop.local>
2023-01-11 12:24:19 +0100

    auth front refactor

--- front/src/context/AuthContext.js
+++ front/src/context/AuthContext.js
@@ -1,173 +1,182 @@
-import React from 'react';
-import {createContext, useState} from 'react';
-import {useHistory} from 'react-router-dom';
+import React from "react";
+import { createContext, useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+import { useHistory } from "react-router-dom";
 
 const AuthContext = createContext();
 
 export default AuthContext;
 
-export const AuthProvider = ({children}) => {
-  const [user, setUser] = useState(
-      () => localStorage.getItem('userProfile') ?
-      JSON.parse(localStorage.getItem('userProfile')) :
-      null,
+export const AuthProvider = ({ children }) => {
+  const [authTokens, setAuthTokens] = useState(() =>
+    localStorage.getItem("authTokens")
+      ? JSON.parse(localStorage.getItem("authTokens"))
+      : null
   );
+  const [user, setUser] = useState(() =>
+    localStorage.getItem("authTokens")
+      ? jwt_decode(localStorage.getItem("authTokens"))
+      : null
+  );
+  let [loading, setLoading] = useState(true);
   const history = useHistory();
 
   const registerUserTeacher = async (e) => {
     e.preventDefault();
-    const response = await fetch(
-        'http://localhost:8000/teacher/register/',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          credentials: 'include',
-          body: JSON.stringify({
-            'user': {
-              'username': e.target.userName.value,
-              'password': e.target.password.value,
-            },
-            'first_name': e.target.firstName.value,
-            'last_name': e.target.lastName.value,
-            'gender': e.target.gender.value,
-          }),
+    const response = await fetch("http://localhost:8000/teacher/register/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        username: e.target.userName.value,
+        password: e.target.password.value,
+        teacher_profile: {
+          first_name: e.target.firstName.value,
+          last_name: e.target.lastName.value,
+          gender: e.target.gender.value,
         },
-    );
+      }),
+    });
     if (response.status === 200) {
-      history.push('/');
-      return '';
+      history.push("/");
+      return "";
     }
     if (response.status === 400) {
-      return 'Ce nom d\'utilisateur existe déjà !';
+      return "Ce nom d'utilisateur existe déjà !";
     }
   };
-
 
   const loginUserStudent = async (e) => {
     e.preventDefault();
 
-    const response = await fetch(
-        'http://localhost:8000/student/register/',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          credentials: 'include',
-          body: JSON.stringify({
-            'user': {
-              'username': e.target.username.value,
-              'password': e.target.classroom_id.value,
-            },
-            'classroom': e.target.classroom_id.value,
-          }),
-        },
-    );
+    const response = await fetch("http://localhost:8000/student/register/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        username: e.target.username.value,
+        password: e.target.classroom_id.value,
+        student_profile: { classroom: e.target.classroom_id.value },
+      }),
+    });
     // If the registration went well, we only log the user
     if (response.status === 200) {
-      const response = await fetch(
-          'http://localhost:8000/student/login/',
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-            body: JSON.stringify({
-              'username': e.target.username.value,
-              'password': e.target.classroom_id.value,
-            }),
-          },
-      );
+      const response = await fetch("http://localhost:8000/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({
+          username: e.target.username.value,
+          password: e.target.classroom_id.value,
+        }),
+      });
       if (response.status === 200) {
-        const response = await fetch(
-            'http://localhost:8000/student/',
-            {
-              method: 'GET',
-              headers: {'Content-Type': 'application/json'},
-              credentials: 'include',
-            });
+        const response = await fetch("http://localhost:8000/student/", {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+        });
         if (response.status === 200) {
           const content = await response.json();
           setUser(content);
-          localStorage.setItem('userProfile', JSON.stringify(content));
+          localStorage.setItem("userProfile", JSON.stringify(content));
         }
-        history.push('/student');
+        history.push("/student");
       } else {
-        alert('EROOR TODO');
+        alert("EROOR TODO");
       }
     }
     // Else if the username is already registered in the room
     if (response.status === 400) {
-      alert('Cet utilisateur est déjà présent dans la classroom');
+      alert("Cet utilisateur est déjà présent dans la classroom");
     }
   };
 
   const loginUserTeacher = async (e) => {
     e.preventDefault();
-    const response = await fetch(
-        'http://localhost:8000/teacher/login/',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          credentials: 'include',
-          body: JSON.stringify({
-            'username': e.target.username.value,
-            'password': e.target.password.value,
-          }),
-        },
-    );
-    if (response.status === 200) {
-      const response = await fetch(
-          'http://localhost:8000/teacher/',
-          {
-            method: 'GET',
-            headers: {'Content-Type': 'application/json'},
-            credentials: 'include',
-          });
-      if (response.status === 200) {
-        const content = await response.json();
-        setUser(content);
-        localStorage.setItem('userProfile', JSON.stringify(content));
-      }
-      history.push('/teacher');
+    const response = await fetch("http://localhost:8000/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        username: e.target.username.value,
+        password: e.target.password.value,
+      }),
+    });
+    let data = await response.json();
+    if (response.status === 200 && jwt_decode(data.access).role == "TEACHER") {
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access));
+      localStorage.setItem("authTokens", JSON.stringify(data));
+      history.push("/teacher");
     } else {
-      alert('EROOR TODO');
+      alert("EROOR TODO");
     }
   };
 
-  const logoutUser = async (e) => {
-    e.preventDefault();
-    const response = await fetch(
-        'http://localhost:8000/logout/', {
-          method: 'GET',
-          headers: {'Content-Type': 'application/json'},
-          credentials: 'include',
-        });
+  const logoutUser = async () => {
+    setUser(null);
+    setAuthTokens(null);
+    localStorage.removeItem("authTokens");
+    history.push("/");
+  };
+
+  const updateToken = async () => {
+    const response = await fetch("http://localhost:8000/login/refresh/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        refresh: authTokens?.refresh,
+      }),
+    });
+    let data = await response.json();
     if (response.status === 200) {
-      setUser(null);
-      localStorage.removeItem('userProfile');
-      history.push('/');
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access));
+      localStorage.setItem("authTokens", JSON.stringify(data));
     } else {
-      alert('EROOR TODO');
+      logoutUser();
+    }
+    if (loading) {
+      setLoading(false);
     }
   };
 
   const contextData = {
     user: user,
+    authTokens: authTokens,
     loginUserTeacher: loginUserTeacher,
     loginUserStudent: loginUserStudent,
     logoutUser: logoutUser,
     registerUserTeacher: registerUserTeacher,
   };
 
+  useEffect(() => {
+    if (loading) {
+      updateToken();
+    }
+    let fourMinutes = 1000 * 60 * 4;
+    let interval = setInterval(() => {
+      if (authTokens) {
+        updateToken();
+      }
+    }, fourMinutes);
+    return () => clearInterval(interval);
+  }, [authTokens, loading]);
+
   return (
     <AuthContext.Provider value={contextData}>
-      {children}
-    </AuthContext.Provider >
+      {loading ? null : children}
+    </AuthContext.Provider>
   );
 };
