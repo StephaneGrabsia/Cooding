5bed56e5735968508ce83659ba70d4cf8d0bc3a6
Sacha BINDER <sacha@Sachas-Laptop.local>
2023-02-14 17:21:12 +0100

    form adaptaion

--- front/src/components/teacherDashboard/ExerciseForm.js
+++ front/src/components/teacherDashboard/ExerciseForm.js
@@ -27,21 +27,19 @@ class ExerciseForm extends React.Component {
     super();
     this.state = {
       fields: {
-        firstName: '',
-        lastName: '',
-        gender: '',
-        userName: '',
-        password: '',
-        passwordConfirmation: '',
-        checkbox: false,
+        statment: this.props.statment,
+        solution: 'bobo\n\nbibi',
+        test_input: '[1,2,3]',
+        correct_output: '[1,2,3]',
+        classroom: '',
       },
       errors: {},
       mainError: '',
     };
     this.handleChange = this.handleChange.bind(this);
-    this.submituserRegistrationForm =
-      this.submituserRegistrationForm.bind(this);
+    this.submitForm = this.submitForm.bind(this);
   }
+
   /**
    * handleChange function
    * set correct values to what user put in fields in state
@@ -49,132 +47,29 @@ class ExerciseForm extends React.Component {
    */
   handleChange(e) {
     const fields = this.state.fields;
-    if (e.target.name === 'checkbox') {
-      fields[e.target.name] = e.target.checked;
-    } else {
-      fields[e.target.name] = e.target.value;
-    }
+    fields[e.target.name] = e.target.value;
     this.setState({fields});
+    console.log(fields);
   }
 
   /**
-   * setErrorMessage function
-   * set a main message error when needed
-   * @argument {string} text : text to display
+   * handleChange for code editor
+   * @argument {value} value : value to set
    */
-  setErrorMessage(text) {
-    const mainError = text;
-    this.setState({
-      mainError: mainError,
-    });
-  }
+  handleEditorChange = (value) => {
+    const fields = this.state.fields;
+    fields['solution'] = value;
+  };
 
   /**
-   * submituserRegistrationForm function
+   * submitForm function
    * called at the submition of the form
    * check if the form is valid and return the error message if there is one
    * @argument {Event} e : event called onSubmit
    */
-  submituserRegistrationForm(e) {
-    const {registerUserTeacher} = this.context;
+  submitForm(e) {
     e.preventDefault();
-    if (this.validateForm()) {
-      registerUserTeacher(e).then((a) => {
-        this.setErrorMessage(a);
-      });
-    }
-  }
-
-  /**
-   * validateForm function
-   * Called when onSubmit and check if the form is valid
-   * @return {boolean} true if its valid, false else
-   */
-  validateForm() {
-    const fields = this.state.fields;
-    const errors = {};
-    let formIsValid = true;
-
-    if (!fields['firstName']) {
-      formIsValid = false;
-      errors['firstName'] = '*Merci d\'entrer un prénom';
-    }
-
-    if (typeof fields['firstName'] !== 'undefined') {
-      if (!fields['firstName'].match(/^[a-zA-Z-]+$/)) {
-        formIsValid = false;
-        errors['firstName'] =
-          '*Merci d\'utiliser uniquement des caractères alphabétiques';
-      }
-    }
-
-    if (!fields['lastName']) {
-      formIsValid = false;
-      errors['lastName'] = '*Merci d\'entrer un nom';
-    }
-
-    if (typeof fields['lastName'] !== 'undefined') {
-      if (!fields['lastName'].match(/^[a-zA-Z-]+$/)) {
-        formIsValid = false;
-        errors['lastName'] =
-          '*Merci d\'utiliser uniquement des caractères alphabétiques';
-      }
-    }
-
-    if (!fields['gender']) {
-      formIsValid = false;
-      errors['gender'] = '*Merci de préciser votre genre';
-    }
-
-    if (!fields['userName']) {
-      formIsValid = false;
-      errors['userName'] = '*Merci d\'entrer un nom d\'utilisateur';
-    }
-
-    if (typeof fields['username'] !== 'undefined') {
-      if (!fields['username'].match(/^[a-zA-Z-]+$/)) {
-        formIsValid = false;
-        errors['username'] =
-          '*Merci d\'utiliser uniquement des caractères alphabétiques';
-      }
-    }
-
-    if (!fields['password']) {
-      formIsValid = false;
-      errors['password'] = '*Merci d\'entrer votre mot de passe';
-    }
-
-    if (typeof fields['password'] !== 'undefined') {
-      if (!fields['password'].match(/^(?=.*[A-Z])(?=.{5,}).*$/)) {
-        formIsValid = false;
-        errors['password'] =
-          '*Merci d\'entrer un mot de passe avec au moins' +
-          '5 caractères et une majuscule';
-      }
-    }
-
-    if (!fields['passwordConfirmation']) {
-      formIsValid = false;
-      errors['passwordConfirmation'] = '*Merci de confirmer votre mot de passe';
-    }
-
-    if (typeof fields['passwordConfirmation'] !== 'undefined') {
-      if (fields['passwordConfirmation'] !== fields['password']) {
-        formIsValid = false;
-        errors['passwordConfirmation'] =
-          '*Les mots de passe ne correspondent pas';
-      }
-    }
-
-    if (!fields['checkbox']) {
-      formIsValid = false;
-      errors['checkbox'] = '*Merci d\'accepter les conditions';
-    }
-
-    this.setState({
-      errors: errors,
-    });
-    return formIsValid;
+    console.log('toto');
   }
 
   /**
@@ -191,13 +86,15 @@ class ExerciseForm extends React.Component {
       <Grid>
         <Paper elevation={0} style={paperStyle}>
           <Grid>
-            <form onSubmit={this.submituserRegistrationForm}>
+            <form onSubmit={this.submitForm}>
               <TextField
                 id="standard-basic"
                 variant="filled"
                 margin="dense"
                 label="Sujet de l'exercice (en MarkDown)"
-                name="statement"
+                name="statment"
+                defaultValue={this.state.fields['statment']}
+                onChange={this.handleChange}
                 fullWidth
                 multiline
                 maxRows={10}
@@ -208,7 +105,8 @@ class ExerciseForm extends React.Component {
               <Grid2 style={codeEditorSize}>
                 <Editor
                   defaultLanguage="python"
-                  defaultValue="#Insert here your code ;)!"
+                  value={this.state.fields['solution']}
+                  onChange={this.handleEditorChange}
                   options={{
                     minimap: {
                       enabled: false,
@@ -225,6 +123,8 @@ class ExerciseForm extends React.Component {
                 margin="dense"
                 label="Input à tester (bien respecter le format)"
                 name="test_input"
+                defaultValue={this.state.fields['test_input']}
+                onChange={this.handleChange}
                 fullWidth
               />
               <TextField
@@ -233,6 +133,8 @@ class ExerciseForm extends React.Component {
                 margin="dense"
                 label="Sortie attendue (Sortie exacte du shell)"
                 name="correct_output"
+                defaultValue={this.state.fields['correct_output']}
+                onChange={this.handleChange}
                 fullWidth
               />
               <Stack direction="row" spacing={2} mt={2}>
